4577a9d4fbb86f7d180bd65bdf1ff70c02922b50
Thomas Benjamin Wester <twester@cubone.cs.uchicago.edu>
2017-02-26 18:42:14 -0600

    coincidence script updated

--- scripts/coincidence.R
+++ scripts/coincidence.R
@@ -17,14 +17,14 @@ data <- cbind(data, total_time)
 
 hist(data$total_time, breaks=seq(0,max(data$total_time)+1, by=0.5), xlim=c(0,120))
 
-i_list <- 800:length(data[,1])
+i_list <- 800:900#length(data[,1])
 j_list <- 1:length(data[,1])
 
 n_inuse = c()
 list_inuse = c()
 for (i in i_list) {
   n_available = 0
-  coin_list = c()
+  coin_list = ""
   print(i)
   for (j in j_list) {
     ## Don't check for coincidence for times after
@@ -42,18 +42,16 @@ for (i in i_list) {
     
     ## Actually check to see if the machine is available at the time
     if (data$start_time[j] + data$total_time[j] * 60 > data$start_time[i]) {
-      if (i == 749) {
-        print(data$start_time[j])
-        print(data$total_time[j])
-        print(data$start_time[i])
-        print(data$number[i])
-        print(data$number[j])
-      }
       n_available = n_available + 1
-      coin_list <- paste(coin_list, toString(data$number[j]))
+      if (coin_list != "") {
+        coin_list <- paste(coin_list, toString(data$number[j]), sep=",")
+      }
+      else {
+        coin_list <- toString(data$number[j])
+      }
     }
   }
   n_inuse <- append(n_open, n_available)
-  list_inuse <- rbind(list_inuse, coin_list)
+  list_inuse <- c(list_inuse, coin_list)
 }
 
