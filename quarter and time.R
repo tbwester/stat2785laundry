eed1fd7ae319d470ccde6647c70624e2ddd460e4
gordonjgz <gordonjgz@gmail.com>
2017-03-11 15:25:18 -0600

    Add files via upload

---
+++ quarter and time.R
@@ -0,0 +1,65 @@
+
+##Use after you read the data
+date=format(as.POSIXct(maxpalcen_data$start_time, format="%Y-%m-%d %H:%M:%S"), format="%d")
+mo=format(as.POSIXct(maxpalcen_data$start_time, format="%Y-%m-%d %H:%M:%S"), format="%m")
+date<-as.numeric(date)
+mo<-as.numeric(mo)
+
+## Divide into differnet part of time
+
+##0-6 Dust
+##6-12 Morning
+##12-18 Afternoon
+##18-24 Night
+
+diff_time_day <-function(hrs){
+  if(hrs>=0&&hrs<6){
+    return(1)
+  }else if(hrs>=6&&hrs<12){
+    return(2)
+  }else if (hrs>=12&&hrs<18){
+    return(3)
+  }else if(hrs>=18&&hrs<24){
+    return(4)
+  }
+}
+
+add_time_seg<-function(hrs){
+  time=c()  
+  for(i in 1:length(hrs)){
+    time[i]=diff_time_day(hrs[i])
+  }
+  return(time)
+}
+
+##Start to devide the machines into different time of the quarter
+##Quarter beginer Sep.24-Oct.14 Wk1 to wk3
+##Midterms Oct.15-Nov.4 Wk 4 to wk6
+##Thanksgiving Nov.5-Nov.25 wk7 to wk 9
+##Reading period and Finals Nov.26 - Dec.10
+
+diff_part_quarter <-function(date, mo){
+  if (mo==9){
+    return(1)
+  }else if (mo==12){
+    return(4)
+  }else if (mo==10 && date<=14){
+    return(1)
+  }else if (mo==10 && date<=15){
+    return(2)
+  }else if(mo==11 && date<=4){
+    return(2)
+  }else if(mo==11 && date >=5 && date <=25){
+    return(3)
+  }else{
+    return(4)
+  }
+}
+
+add_quarter_seg<-function(date,mo){
+  quarter=c()  
+  for(i in 1:length(date)){
+    quarter[i]=diff_part_quarter(date[i],mo[i])
+  }
+  return(quarter)
+}
